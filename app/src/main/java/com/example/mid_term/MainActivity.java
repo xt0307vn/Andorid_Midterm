07adfe471f91a3a5a4954e655d6e72005c0333ba
Xuân Trường <xt0307vn@gmail.com>
2022-09-26 09:44:29 +0700

    day 26/09/22 9:44

---
+++ app/src/main/java/com/example/mid_term/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mid_term;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
