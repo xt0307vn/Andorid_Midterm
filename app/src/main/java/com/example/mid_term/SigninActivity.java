38f3426badc92f1143d23ffb26409078b228fac5
Xuân Trường <xt0307vn@gmail.com>
2022-09-27 12:41:53 +0700

    day 27/09/22 12:41pm

--- app/src/main/java/com/example/mid_term/SigninActivity.java
+++ app/src/main/java/com/example/mid_term/SigninActivity.java
@@ -8,18 +8,9 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class SigninActivity extends AppCompatActivity {
-    RelativeLayout signinRlt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        signinRlt = findViewById(R.id.signinRlt);
-
-        signinRlt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(SigninActivity.this, "OK", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
