3f92b4fa7c16685493c70471bfef4dce2ac83dbe
Xuân Trường <xt0307vn@gmail.com>
2022-09-30 16:01:15 +0700

    day 30/09/22 4:01pm

--- app/src/main/java/com/example/mid_term/LearndetailActivity.java
+++ app/src/main/java/com/example/mid_term/LearndetailActivity.java
@@ -9,6 +9,6 @@ public class LearndetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learndetail);
+        setContentView(R.layout.activity_coursedetail);
     }
 }
